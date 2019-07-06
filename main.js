cba455a12c3f5f8edeeae4bdad4d14959151d417
VishalVrk <vishalvrk97@gmail.com>
2019-07-06 19:42:00 +0530

    Added Mobile Css

---
+++ main.js
@@ -0,0 +1,16 @@
+//Stick Navbar
+
+
+let navbar = $(".navbar");
+
+$(window).scroll(function(){
+    let oTop = $(".section-2").offset().top - window.innerHeight;
+    if($(window).scrollTop()>oTop){
+        navbar.addClass("sticky");
+    }
+    else{
+        navbar.removeClass("sticky")
+    }
+}
+    
+);
